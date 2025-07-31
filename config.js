acd9ad10a28038de0ea53816c8cebfee15eeb91f
Ibino273 <lorenzo.delgiudice.sc@gmail.com>
2025-08-01 00:01:42 +0200

    Update config.js
    Cambiati i numeri per provare integrazione Supabase

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js
 module.exports = {
   BASE_URL: 'https://www.subito.it/annunci-piemonte/vendita/moto-e-scooter/',
-  MAX_PAGES_TO_SCRAPE: 3, // Numero massimo di pagine da scansionare
-  MAX_LISTINGS_PER_PAGE_DEBUG: 2, // Numero massimo di annunci da elaborare per pagina in modalità debug
-  MAX_TOTAL_LISTINGS_DEBUG: 6, // Numero massimo totale di annunci da elaborare (es. 2 annunci/pagina * 3 pagine = 6)
+  MAX_PAGES_TO_SCRAPE: 2, // Numero massimo di pagine da scansionare
+  MAX_LISTINGS_PER_PAGE_DEBUG: 999, // Numero massimo di annunci da elaborare per pagina in modalità debug
+  MAX_TOTAL_LISTINGS_DEBUG: 50, // Numero massimo totale di annunci da elaborare (es. 2 annunci/pagina * 3 pagine = 6)
   LOG_FILE_PATH: 'scraper.log', // Percorso del file di log
 };
